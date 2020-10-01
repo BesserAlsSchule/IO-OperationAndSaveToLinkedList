476ab014d8311210e89895294f8d005357ad2083
Radagast-dev <57732837+Radagast-dev@users.noreply.github.com>
2020-10-01 17:19:34 +0200

    Update IO-OperationAndSaveToLinkedList.cpp

--- IO-OperationAndSaveToLinkedList/IO-OperationAndSaveToLinkedList/IO-OperationAndSaveToLinkedList.cpp
+++ IO-OperationAndSaveToLinkedList/IO-OperationAndSaveToLinkedList/IO-OperationAndSaveToLinkedList.cpp
@@ -1,18 +1,15 @@
-//#include <iostream>				//IO Output stream object lib -> c++
 #include <stdlib.h>					//malloc(), free(), mem mngmt
 #include <stdio.h>					//file related IO, putchar(), gets
 #include <Windows.h>				//console
-//#include <string.h>				//macros+functions für char arrays (Zeichenketten)
 
 int main()
 {
 	//Bufferdeklarationen
 	SetConsoleTitleA("Enter Text and save to linked List");
-	system("color 2");											//https://ss64.com/nt/
 
-	char inputStrBuffer[20] = {};		//char Buffer mit leerer menge initialisieren für freigeräumten speicher
-	char inputStrBuffer2[20] = {};		//nr 2 für element 2
-	char inputStrBuffer3[20] = {};		//nr 3 für element 3
+	char inputStrBuffer[3] = {0};		//char Buffer mit leerer menge initialisieren für freigeräumten speicher
+	char inputStrBuffer2[3] = {0};		//nr 2 für element 2
+	char inputStrBuffer3[3] = {0};		//nr 3 für element 3
 
 
 	//Start IO Operations 
@@ -20,7 +17,7 @@ int main()
 	printf("\n");
 	printf("\n");
 
-	printf("Please enter your Elements via Keyboard");
+	printf("Please enter your single char via Keyboard");
 	printf("\n");
 
 	fgets(inputStrBuffer, sizeof(inputStrBuffer), stdin);		//fgets(buffer, größe, stream/input) seit c++11 raus -> fgets bessere lösung wgn compilersicherheit
@@ -29,7 +26,7 @@ int main()
 	printf(inputStrBuffer);
 	printf("\n");
 
-	//gebe+printe input 2 + input 3
+	//give+print input 2 + input 3
 	fgets(inputStrBuffer2, sizeof(inputStrBuffer2), stdin);
 	printf("\n");
 	printf("Your 2nd Input was: ");
@@ -45,7 +42,7 @@ int main()
 	//LinkedList
 	typedef struct element												//definiere (typedef struct "element") <typ> struct element
 	{
-		char valueBuffer[20] = { };										//node value
+		char valueBuffer[3] = {0};										//node value
 		element* nextPtr;												//ptr to next element
 	} nodeElement;														//deklariere (struct) nodeElement vom <datentyp> element <bezeichner für neu definierten type struct> element
 
@@ -55,15 +52,15 @@ int main()
 
 	element.nextPtr = &element2;										//pointe auf nächstes element
 	element2.nextPtr = &element3;
-	element3.nextPtr = 0;												//ende der linked list
+	element3.nextPtr = nullptr;
 
-	//Doppelalloc
-	//fgets(element.valueBuffer, sizeof(inputStrBuffer), stdin);		    //schreibe herein umgehe damit malloc?
-	//fgets(element2.valueBuffer, sizeof(inputStrBuffer2), stdin);
-	//fgets(element3.valueBuffer, sizeof(inputStrBuffer3), stdin);
+	//copy inputStrBuffer to element.values
+	*element.valueBuffer = *inputStrBuffer;
+	*element2.valueBuffer = *inputStrBuffer2;
+	*element3.valueBuffer = *inputStrBuffer3;
 
 	printf("\n");
-	printf("Input given, press ENTER to continue");
+	printf("Input given");
 	printf("\n");
 
 	system("Pause");													//getkeyboard
@@ -73,29 +70,39 @@ int main()
 
 	if (element.nextPtr && element2.nextPtr != 0)						//NULLprüfung
 	{
-		printf("Elements ready and linked !");
+		system("cls");
+		system("color 2");
+		
+		//print values
+		printf("List elements ready and linked, printing elements values");
+		printf("\n");
+		printf(element.valueBuffer);
+		printf("\n");
+		printf(element2.valueBuffer);
+		printf("\n");
+		printf(element3.valueBuffer);
+		printf("\n");
+		printf("\n");
+
+		//print memory adress of values
+		printf("Printing adress stored in linked list elements");
+		printf("\n");
+		printf("%p", element.valueBuffer);
+		printf("\n");
+		printf("%p", element2.valueBuffer);
+		printf("\n");
+		printf("%p", element3.valueBuffer);
 		printf("\n");
 	}
 	else
 	{
-		printf("Element creation of linked lists failed !");
+		printf("Linked lists element creation failed !");
 		printf("\n");
 	}
 
-	//user answer, print input
-	printf("Enter Y or y for printing values of linked list elements to console");
-	char consoleAnswerBufferY[4] = "Y y";
-
-	if (scanf((consoleAnswerBufferY) = "Y y")) 
-	{
-
-	}
-	
-		
 	printf("\n");
-	printf("Enter N or n for printing values of linked list elements to console");
-	char consoleAnswerBufferN[4] = "N n";
+	printf("End");
 	printf("\n");
 
 	return 0;
-}
+};
