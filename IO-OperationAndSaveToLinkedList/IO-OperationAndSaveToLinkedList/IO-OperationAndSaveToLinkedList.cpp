b7ac12fe04604bde6338f7ef9e300f01d814af6f
Smitty Werben <smittywerbenjj.1@gmail.com>
2020-10-02 09:22:09 +0200

    Projekt suaber gesäubert
    Mehthoden Extrahiert

--- IO-OperationAndSaveToLinkedList/IO-OperationAndSaveToLinkedList/IO-OperationAndSaveToLinkedList.cpp
+++ IO-OperationAndSaveToLinkedList/IO-OperationAndSaveToLinkedList/IO-OperationAndSaveToLinkedList.cpp
@@ -1,17 +1,174 @@
 #include <stdlib.h>					//malloc(), free(), mem mngmt
 #include <stdio.h>					//file related IO, putchar(), gets
 #include <Windows.h>				//console
+#include "IO-OperationAndSaveToLinkedList.h"
+#include <stdbool.h>
+
+
+
+char inputStrBuffer[3] = { 0 };		//char Buffer mit leerer menge initialisieren für freigeräumten speicher
+
+
+//LinkedList
+typedef struct element												//definiere (typedef struct "element") <typ> struct element
+{
+	char valueBuffer[3] = { };
+	element* nextPtr = nullptr;										//ptr to next element
+} nodeElement;
+
+nodeElement rootNode;
 
 int main()
 {
-	//Bufferdeklarationen
+	
+	bool exitProgram = false;
+	
 	SetConsoleTitleA("Enter Text and save to linked List");
 
-	char inputStrBuffer[3] = {0};		//char Buffer mit leerer menge initialisieren für freigeräumten speicher
-	char inputStrBuffer2[3] = {0};		//nr 2 für element 2
-	char inputStrBuffer3[3] = {0};		//nr 3 für element 3
+	//deklariere (struct) nodeElement vom <datentyp> element <bezeichner für neu definierten type struct> element
 
 
+#pragma region MAIN MENU
+
+
+
+
+
+	printf("===== HAUPTEMENUE =====\n");
+
+	char input;
+
+	while (!exitProgram) { // DAS EIGENTLICHE PROGRAMM (IN ENDLOSSCHLEIFE)
+		printf("1. Ein Element Hinzufuegen.\n");
+		printf("2. Die Liste Ausgeben.\n");
+		printf("x. Programm Beenden\n");
+		printf("Eingabe: ");
+
+		//https://stackoverflow.com/questions/2279379/how-to-convert-integer-to-char-in-c
+
+		//while (fgets(&input, 1, stdin)) 	//HOL TEXT AUS STDIN UND SCHMEISS IN ADDRESSE VON INPUT
+		while (input = getc(stdin)) 	//HOL EIN CHAR AUS STDIN UND SCHMEISS IN ADDRESSE VON INPUT ->  http://www.cplusplus.com/reference/cstdio/getc/
+		{
+			if (input == 'x') {
+				exitProgram = true;
+				break;
+			}
+			else if (input == '1') {
+				//Element Hinzufügen
+				AddElementToLinkedList();
+			}
+			else if (input == '2') {
+				//Liste Ausgeben
+				PrintLinkedList();
+			}
+			
+		}
+
+	}
+#pragma endregion
+
+	// Free Allocated Memory
+
+	//CLose the Program
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#pragma region LOGIK
+
+
+
+//if (element.nextPtr && element2.nextPtr != 0)						//NULLprüfung
+//{
+//	system("cls");
+//	system("color 2");
+
+//	//print values
+//	printf("List elements ready and linked, printing elements values");
+//	printf("\n");
+//	printf(element.valueBuffer);
+//	printf("\n");
+//	printf(element2.valueBuffer);
+//	printf("\n");
+//	printf(element3.valueBuffer);
+//	printf("\n");
+//	printf("\n");
+
+//	//print memory adress of values
+//	printf("Printing adress stored in linked list elements");
+//	printf("\n");
+//	printf("%p", element.valueBuffer);
+//	printf("\n");
+//	printf("%p", element2.valueBuffer);
+//	printf("\n");
+//	printf("%p", element3.valueBuffer);
+//	printf("\n");
+//}
+//else
+//{
+//	printf("Linked lists element creation failed !");
+//	printf("\n");
+//}
+
+
+
+#pragma endregion
+
+
+void PrintLinkedList() {
+	printf(" Anweisungen um ein Die Liste Anzuzeigen\n");
+}
+
+void AddElementToLinkedList() {
+	printf(" Anweisungen um ein Element Hinzuzufügen\n");
+
+	GetUserInput();
+
+	// Ein Listeneelement Erstellen (malloc) und Die Adresse des Reservierten Speichers in einen ZwischenPointe ablegen
+	nodeElement* pNewNode;
+	pNewNode = (nodeElement*)malloc(sizeof(pNewNode));
+
+	//Das Letzte Listenelement Holen und den Erstellten Speicherbereich Verweisen
+	/* Von der Root node anfangen das Feld nextPTr zu prüfen bis es einen nullpointern vorzeigt, dieser Node ist dann der letzte
+	* Node
+	*/
+	
+	nodeElement* currentNodeElement = &rootNode;
+	while (currentNodeElement->nextPtr != nullptr) {
+		//currentNextPTr = nextPtr des Nächsten nodes
+		currentNodeElement = &(*(currentNodeElement->nextPtr));
+	}
+	// Adressee des neu Erstelltes Listenelement ,dem vorherigen Listenelemnt in (nextPtr) reinschreiben
+
+#pragma region USER INPUT
+
+
+
+#pragma endregion
+}
+
+
+/// <summary>
+/// eingabe Vom User in inputStrBuffer Schreiben
+/// </summary>
+/// <param name="inputStrBuffer"></param>
+void GetUserInput()
+{
 	//Start IO Operations 
 	printf("Hello C LinkedList!");
 	printf("\n");
@@ -19,90 +176,17 @@ int main()
 
 	printf("Please enter your single char via Keyboard");
 	printf("\n");
+	
+	while (fgets(inputStrBuffer, sizeof(inputStrBuffer), stdin) != nullptr) {
+		break;
+	}
+	
+			//fgets(buffer, größe, stream/input) seit c++11 raus -> fgets bessere lösung wgn compilersicherheit
 
-	fgets(inputStrBuffer, sizeof(inputStrBuffer), stdin);		//fgets(buffer, größe, stream/input) seit c++11 raus -> fgets bessere lösung wgn compilersicherheit
 	printf("\n");
-	printf("Your 1st Input was: ");
+	printf("Your Input was: ");
 	printf(inputStrBuffer);
 	printf("\n");
+}
 
-	//give+print input 2 + input 3
-	fgets(inputStrBuffer2, sizeof(inputStrBuffer2), stdin);
-	printf("\n");
-	printf("Your 2nd Input was: ");
-	printf(inputStrBuffer2);
-	printf("\n");
 
-	fgets(inputStrBuffer3, sizeof(inputStrBuffer3), stdin);
-	printf("\n");
-	printf("Your 3rd Input was: ");
-	printf(inputStrBuffer3);
-	printf("\n");
-
-	//LinkedList
-	typedef struct element												//definiere (typedef struct "element") <typ> struct element
-	{
-		char valueBuffer[3] = {0};										//node value
-		element* nextPtr;												//ptr to next element
-	} nodeElement;														//deklariere (struct) nodeElement vom <datentyp> element <bezeichner für neu definierten type struct> element
-
-	nodeElement element;												//deklariere instanz vom typ element
-	nodeElement element2;
-	nodeElement element3;
-
-	element.nextPtr = &element2;										//pointe auf nächstes element
-	element2.nextPtr = &element3;
-	element3.nextPtr = nullptr;
-
-	//copy inputStrBuffer to element.values
-	*element.valueBuffer = *inputStrBuffer;
-	*element2.valueBuffer = *inputStrBuffer2;
-	*element3.valueBuffer = *inputStrBuffer3;
-
-	printf("\n");
-	printf("Input given");
-	printf("\n");
-
-	system("Pause");													//getkeyboard
-
-	printf("\n");
-	printf("\n");
-
-	if (element.nextPtr && element2.nextPtr != 0)						//NULLprüfung
-	{
-		system("cls");
-		system("color 2");
-		
-		//print values
-		printf("List elements ready and linked, printing elements values");
-		printf("\n");
-		printf(element.valueBuffer);
-		printf("\n");
-		printf(element2.valueBuffer);
-		printf("\n");
-		printf(element3.valueBuffer);
-		printf("\n");
-		printf("\n");
-
-		//print memory adress of values
-		printf("Printing adress stored in linked list elements");
-		printf("\n");
-		printf("%p", element.valueBuffer);
-		printf("\n");
-		printf("%p", element2.valueBuffer);
-		printf("\n");
-		printf("%p", element3.valueBuffer);
-		printf("\n");
-	}
-	else
-	{
-		printf("Linked lists element creation failed !");
-		printf("\n");
-	}
-
-	printf("\n");
-	printf("End");
-	printf("\n");
-
-	return 0;
-};
---
+++ IO-OperationAndSaveToLinkedList/IO-OperationAndSaveToLinkedList/Quelle.c
@@ -0,0 +1,185 @@
+//#include <stdlib.h>					//malloc(), free(), mem mngmt
+//#include <stdio.h>					//file related IO, putchar(), gets
+//#include <Windows.h>				//console
+//#include "IO-OperationAndSaveToLinkedList.h"
+//#include <stdbool.h>
+//
+//
+//
+//char inputStrBuffer[3] = {0};		//char Buffer mit leerer menge initialisieren für freigeräumten speicher
+//
+//
+////LinkedList
+//typedef struct element												//definiere (typedef struct "element") <typ> struct element
+//{
+//	char valueBuffer[3];
+//	nodeElement* nextPtr;// = nullptr;										//ptr to next element
+//} nodeElement;
+//
+//
+//int main()
+//{
+//	bool exitProgram = false;
+//	SetConsoleTitleA("Enter Text and save to linked List");
+//
+//	//deklariere (struct) nodeElement vom <datentyp> element <bezeichner für neu definierten type struct> element
+//
+//
+//#pragma region MAIN MENU
+//
+//
+//
+//
+//
+//	printf("===== HAUPTEMENUE =====\n");
+//
+//	char input;
+//
+//	while (!exitProgram) { // DAS EIGENTLICHE PROGRAMM (IN ENDLOSSCHLEIFE)
+//		printf("1. Ein Element Hinzufuegen.\n");
+//		printf("2. Die Liste Ausgeben.\n");
+//		printf("x. Programm Beenden\n");
+//		printf("Eingabe: ");
+//
+//		//https://stackoverflow.com/questions/2279379/how-to-convert-integer-to-char-in-c
+//
+//		//while (fgets(&input, 1, stdin)) 	//HOL TEXT AUS STDIN UND SCHMEISS IN ADDRESSE VON INPUT
+//		while (input = getc(stdin)) 	//HOL EIN CHAR AUS STDIN UND SCHMEISS IN ADDRESSE VON INPUT ->  http://www.cplusplus.com/reference/cstdio/getc/
+//		{
+//			if (input == 'x') {
+//				exitProgram = true;
+//				break;
+//			}
+//			else if (input == '1') {
+//				//Element Hinzufügen
+//				AddElementToLinkedList();
+//			}
+//			else if (input == '2') {
+//				//Liste Ausgeben
+//				PrintLinkedList();
+//			}
+//		}
+//
+//	}
+//#pragma endregion
+//
+//	// Free Allocated Memory
+//
+//	//CLose the Program
+//	return 0;
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//#pragma region LOGIK
+//
+//
+//
+////if (element.nextPtr && element2.nextPtr != 0)						//NULLprüfung
+////{
+////	system("cls");
+////	system("color 2");
+//
+////	//print values
+////	printf("List elements ready and linked, printing elements values");
+////	printf("\n");
+////	printf(element.valueBuffer);
+////	printf("\n");
+////	printf(element2.valueBuffer);
+////	printf("\n");
+////	printf(element3.valueBuffer);
+////	printf("\n");
+////	printf("\n");
+//
+////	//print memory adress of values
+////	printf("Printing adress stored in linked list elements");
+////	printf("\n");
+////	printf("%p", element.valueBuffer);
+////	printf("\n");
+////	printf("%p", element2.valueBuffer);
+////	printf("\n");
+////	printf("%p", element3.valueBuffer);
+////	printf("\n");
+////}
+////else
+////{
+////	printf("Linked lists element creation failed !");
+////	printf("\n");
+////}
+//
+//
+//
+//#pragma endregion
+//
+//
+//void PrintLinkedList() {
+//	printf(" Anweisungen um ein Die Liste Anzuzeigen\n");
+//}
+//
+//void AddElementToLinkedList() {
+//	printf(" Anweisungen um ein Element Hinzuzufügen\n");
+//
+//	GetUserInput(inputStrBuffer);
+//
+//	// Ein Listeneelement Erstellen (malloc, oder so)
+//	nodeElement newNode;
+//	nodeElement* pNewNode;
+//	pNewNode = (struct nodeElement*)malloc(sizeof(newNode));
+//
+//	// Adressee des neu Erstelltes Listenelement ,dem vorherigen Listenelemnt in (nextPtr) reinschreiben
+//
+//#pragma region USER INPUT
+//
+//
+//
+//#pragma endregion
+//
+//#pragma region Erstes Element der Liste erstellen
+//
+//	nodeElement element;	//deklariere instanz vom typ element
+//
+//	//copy inputStrBuffer to element.values _ OK
+//	*element.valueBuffer = *inputStrBuffer;
+//
+//
+//#pragma endregion
+//}
+//
+//
+///// <summary>
+///// 
+///// </summary>
+///// <param name="inputStrBuffer"></param>
+//void GetUserInput(char  inputStrBuffer[3])
+//{
+//	//Start IO Operations 
+//	printf("Hello C LinkedList!");
+//	printf("\n");
+//	printf("\n");
+//
+//	printf("Please enter your single char via Keyboard");
+//	printf("\n");
+//
+//	fgets(inputStrBuffer, sizeof(inputStrBuffer), stdin);		//fgets(buffer, größe, stream/input) seit c++11 raus -> fgets bessere lösung wgn compilersicherheit
+//
+//	printf("\n");
+//	printf("Your Input was: ");
+//	printf(inputStrBuffer);
+//	printf("\n");
+//}
+//
+//
